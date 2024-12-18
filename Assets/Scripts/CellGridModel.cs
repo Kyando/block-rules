a14cc414566e826e0532b12df83a2b8de4452a9b
Bruno Ribeiro <Brunoribeiro47@hotmail.com>
2024-12-18 19:03:15 -0300

    wip

---
+++ Assets/Scripts/CellGridModel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class CellGridModel
+{
+    public bool isEmpty = false;
+    public Vector2 gridPosition = Vector2.zero;
+    public Color color = Color.black;
+}
--- Assets/Scripts/GridModel.cs
+++ Assets/Scripts/GridModel.cs
@@ -4,21 +4,24 @@ public class GridModel : MonoBehaviour
 {
     [SerializeField] private int _gridWidth = 2;
     [SerializeField] private int _gridHeight = 3;
+
     private Color[,] _grid;
     public GameObject cellPrefab;
+    public Camera camera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector3 positionOffset = new Vector3(-(_gridWidth * 0.5f), -(_gridHeight * 0.5f), 0);
+        this.transform.position = new Vector3(-.05f, .05f, 0);
+        camera.transform.position = new Vector3(_gridWidth * 0.5f, _gridHeight * 0.5f, camera.transform.position.z);
         _grid = new Color[_gridWidth, _gridHeight];
-        Color[] row_colors = {Color.red, Color.green, Color.blue,};
+        Color[] row_colors = { Color.red, Color.green, Color.blue, };
         for (int y = 0; y < _gridHeight; y++)
         {
             for (int x = 0; x < _gridWidth; x++)
             {
                 _grid[x, y] = row_colors[y];
-                Vector3 position = new Vector3(x + positionOffset.x, y + positionOffset.y, 0);
+                Vector3 position = new Vector3(x, y, 0);
                 GameObject go = Instantiate(cellPrefab, position, Quaternion.identity, this.transform);
                 go.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = _grid[x, y];
             }
